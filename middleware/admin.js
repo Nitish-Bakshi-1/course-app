b0a317dd1b124b51a4e7d52b67b341a8a51ffe4e
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-14 01:20:53 +0530

    added checks for token

--- middleware/admin.js
+++ middleware/admin.js
@@ -6,16 +6,17 @@ function adminMiddleware(req, res, next) {
   const words = token.split(" ");
   const jwt_token = words[1];
 
-  const verification = jwt.verify(jwt_token, JWT_SECRET);
-  console.log(verification.username);
+  if (words.length === 2 && words[1] === "Bearer") {
+    const verification = jwt.verify(jwt_token, JWT_SECRET);
 
-  if (!verification) {
-    res.json({
-      message: "(Admin) authentication failed",
-    });
-  } else {
-    req.username = verification.username;
-    next();
+    if (!verification) {
+      res.json({
+        message: "(Admin) authentication failed",
+      });
+    } else {
+      req.username = verification.username;
+      next();
+    }
   }
 }
 
